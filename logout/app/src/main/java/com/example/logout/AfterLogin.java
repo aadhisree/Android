07ca5fc0bc55ed3380bc8d0090d007436cbd9705
aadhisree <adityasreelakshmi@gmail.com>
2024-09-11 14:16:44 +0530

    first commit

--- logout/app/src/main/java/com/example/logout/AfterLogin.java
+++ logout/app/src/main/java/com/example/logout/AfterLogin.java
@@ -1,9 +1,9 @@
-package com.example.facebook;
+package com.example.logout;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.net.Uri;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +11,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AfterLogin extends AppCompatActivity {
+    TextView TV1;
+    TextView TV2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/login?_rdr=p"));
-                startActivity(browserIntent);
-            }
-        });
+        setContentView(R.layout.activity_after_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TV1=(TextView) findViewById(R.id.t1);
+        TV2=(TextView) findViewById(R.id.t2);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            String user = bundle.getString("Uname");
+            String pass = bundle.getString("Pass");
+            TV1.setText("hi "+user);
+            TV2.setText("Your password is "+pass);
+        }
+
+
+    }
+
+    public void logout(View view) {
+        startActivity(new Intent(AfterLogin.this, MainActivity.class));
     }
 }
